b0dbabc400e6acb21bbe5f1796891727d77a066f
vigimus <160164175+vigimus@users.noreply.github.com>
2024-03-26 19:05:02 +0100

    Add files via upload

---
+++ javaScript_Variabler_Del2.js
@@ -0,0 +1,20 @@
+// lägg till variablerna "forstNamn" and "efterNamn" // så det bildar ditt namn
+var forstNamn = "Viktor"; //Viktigt att inte glömma "" när det berör sträng
+var efterNamn = "Larsson";
+// Skapa en variabel för svaret
+var namn = forstNamn + " " + efterNamn;
+
+// Förklara vad som kommer hända med variabeln b i nedan fall?
+var a = 34;
+var b = 21;
+a = 2;
+a + b // Vad blir svaret här? 
+//Svaret kommer bli 23 sammanlagt eftersom till en början sparades världet 23 i 
+//a variabeln sedan ändrades världet igen till 2 vilket är det senaste världen
+//därmed tar den över det den förra a variabeln så innan var svaret 55 nu blev det istället 23
+
+// Vad kommer c att vara?
+var c;
+
+//I det här fallet är c "undefined" det har inte fått ett värde tilldelat på sig
+
